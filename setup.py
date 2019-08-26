f4931c4047dde408b20dfb0c6786b0f1e1b0cd7f
Alexander Reynolds <ar@reynoldsalexander.com>
2019-08-26 16:49:51 -0700

    Update setup.py
    bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="timingdiagrams",
-    version="0.2.0",
+    version="0.2.1",
     author="Alexander Reynolds",
     install_requires=['sortedcollections'],
     packages=find_packages(),

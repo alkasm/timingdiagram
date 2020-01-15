33fa573171cf5609fcb953adbf456e1b75c99af6
alkasm <ar@reynoldsalexander.com>
2020-01-15 00:53:38 -0800

    update for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
 setup(
-    name="timingdiagram",
-    version="0.3.0",
+    name="timingdiagram-alkasm",
+    version="0.3.1",
     author="Alexander Reynolds",
+    url="https://github.com/alkasm/timingdiagram",    
     install_requires=["sortedcollections"],
     packages=find_packages(),
     description="Temporal boolean algebra",
+    long_description=readme,
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
